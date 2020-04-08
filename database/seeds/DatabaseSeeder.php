09c0529d57bfeee7565e5ab1b4813565f48da9b1
StephenPaul <pauldiwata@gmail.com>
2020-04-08 18:46:12 +0800

    Generate fake answers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Question;
+use App\Answer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(User::class,3)->create()->each(function($u){
+        factory(App\User::class,3)->create()->each(function($u){
             $u->questions()
                 ->saveMany(
                 factory(Question::class,rand(1,5))->make()
-            );
+                 )
+              ->each(function ($q) {
+                $q->answers()->saveMany(factory(Answer::class, rand(1, 5))->make());
+              });
         });
 
     }
